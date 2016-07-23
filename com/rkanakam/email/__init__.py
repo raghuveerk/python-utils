57364de14954fcc8cd4ce4a848efb901929f14b3
raghuveerk <veeru.kanakamedala@gmail.com>
2016-07-22 20:49:00 -0400

    send email from a template

--- com/rkanakam/email/__init__.py
+++ com/rkanakam/email/__init__.py
@@ -2,7 +2,7 @@ from smtplib import SMTP              # sending email
 from email.mime.text import MIMEText  # constructing messages
 
 from jinja2 import Environment        # Jinja2 templating
-
+from jinja2 import FileSystemLoader
 
 TEMPLATE = """
 <html>
@@ -16,17 +16,21 @@ TEMPLATE = """
 </body>
 </html>
 """  # Our HTML Template
+subject = "Dengi tagutunnara!!!"
+sender= "raghuveer.kanakamedala@gmail.com"
+recipient = "raghuveerk@outlook.com, pradeepv.datla@gmail.com"
+# Specify any input variables to the template as a dictionary.
+templateVars = { "name" : "Pedda raju..."}
+TEMPLATE_FILE = "/Users/raghuveerk/workspace/python-utils/python-utils/com/rkanakam/email/templates/sample_template.html"
+
+templateLoader = FileSystemLoader( searchpath= "/" )
+templateEnv = Environment(loader=templateLoader)
+template = templateEnv.get_template( TEMPLATE_FILE )
+outputText = template.render( templateVars )
 
 # Create a text/html message from a rendered template
-msg = MIMEText(
-    Environment().from_string(TEMPLATE).render(
-        title='Hello World!'
-    ), "html"
-)
+msg = MIMEText(outputText, "html")
 
-subject = "Subject Line"
-sender= "raghuveer.kanakamedala@gmail.com"
-recipient = "raghuveerk@outlook.com"
 
 msg['Subject'] = subject
 msg['From'] = sender
